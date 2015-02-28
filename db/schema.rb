b4ce9a62fb8f0dee091aa0c7a6e0d49258400f9f
Eric Tillberg <eric.tillberg@gmail.com>
2015-02-28 11:39:25 -0500

    Trying to fix M:M stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226194523) do
+ActiveRecord::Schema.define(version: 20150228160926) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20150226194523) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer "category_it"
+    t.integer "post_id"
   end
 
   create_table "posts", force: true do |t|

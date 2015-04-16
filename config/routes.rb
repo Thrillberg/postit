92e9b5da41d98de6c2976115b7c2763d4222fd5a
Eric Tillberg <eric.tillberg@gmail.com>
2015-04-16 11:53:59 -0400

    Big commit. Almost done with Week 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  resources :categories, only: [:create, :new, :show]
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
